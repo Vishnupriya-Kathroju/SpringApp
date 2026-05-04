f5b1bda7388d79aeba9e1a35547449d39111d625
Vishnupriya-Kathroju <kathrojuvishnupriya@gmail.com>
2026-05-05 00:21:10 +0530

    my first upload to github

---
+++ src/main/java/com/priya/springapp/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.priya.springapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/priya")
+public class DemoController {
+
+	@GetMapping("/isabadgirl")
+	public String demo() {
+		return "hii! the server is running";
+	}
+}
